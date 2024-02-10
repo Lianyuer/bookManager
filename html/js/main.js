f07b04abedd9449bd7fd2fa22f942386a66bcaff
lianyu <1319764245@qq.com>
2024-02-10 23:42:25 +0800

    树状菜单实现，点击一级菜单折叠与展开（使用映射*****）

--- html/js/main.js
+++ html/js/main.js
@@ -1,29 +1,89 @@
-// 生成树形结构菜单
 function showMenu() {
+  // 发起 AJAX 请求，从服务器获取所有菜单项数据
   $.ajax({
     url: "http://localhost:3000/pages/selectAll",
     success: (res) => {
-      console.log(res);
-      let ones = res.filter((item) => {
-        return item.pid == 0;
-      });
-      console.log(ones, "ones");
-      let content = "";
-      // 一级展示
-      ones.forEach((element, index) => {
-        content += `<li data-toggle="collapse" href="#collapseMenu-${index}" role="button" aria-expanded="true" aria-controls="collapseMenu-${index}"><a href="#">${element.pageName}</a><span class="iconfont icon-shuzhuang shouqi"></span>`;
-        // 对应二级
-        let twos = res.filter((item) => {
-          return item.pid == element.id;
-        });
-        console.log(twos, "twos");
-        content += `<ol class='collapse in' id='collapseMenu-${index}'>`;
-        twos.forEach((item) => {
-          content += `<a href="#">${item.pageName}</a>`;
-        });
-        content += `</ol></li>`;
-      });
-      $("#ulShow").append(content);
+      console.log(res, "res");
+      // 创建一个映射对象，用于存储菜单项及其子菜单项，map是计算结束后返回的值，是下一次调用回调时的第一个参数；item是当前值
+      const itemMap = res.reduce((map, item) => {
+        // 将每个菜单项添加到映射中，并初始化其子菜单项数组
+        map[item.id] = { ...item, subItems: [] };
+
+        // 如果菜单项没有父级（顶级菜单项），则标记为顶级
+        if (item.pid === 0) {
+          map[item.id].topLevel = true;
+        } else {
+          // 如果菜单项有父级，则将其添加到父级菜单项的子菜单项数组中
+          const parentId = item.pid;
+          if (map[parentId]) {
+            map[parentId].subItems.push(item);
+          }
+        }
+
+        return map;
+      }, {});
+      console.log(itemMap, "itemMap");
+
+      // 从映射中提取所有顶级菜单项，返回一个新数组
+      const topLevelItems = Object.values(itemMap).filter(
+        (item) => item.topLevel
+      );
+
+      // 构建顶级菜单项的 HTML 内容
+      const listGroupContent = topLevelItems
+        .map((topItem) => {
+          let subItemsHtml = "";
+
+          // 如果顶级菜单项有子菜单项，则构建子菜单项的 HTML 内容
+          if (topItem.subItems.length > 0) {
+            // 为子菜单项创建一个唯一的 collapse ID
+            const collapseId = `collapse-${topItem.id}`;
+
+            // 构建子菜单项的列表组，并将其包装在一个 collapse 元素中
+            subItemsHtml =
+              `<div class="collapse" id="${collapseId}">` +
+              `<ul class="list-group list-group-flush">` +
+              topItem.subItems
+                .map(
+                  (subItem) =>
+                    // 将子菜单项转换为 a 标签，并设置 href 属性
+                    `<li class="list-group-item subItem" onclick="handleSubItem('${subItem.pageName}')"><a href="#">${subItem.pageName}</a></li>`
+                )
+                .join("") +
+              `</ul>` +
+              `</div>`;
+
+            // 创建折叠触发器的按钮，并设置 data-target 属性以指向相应的 collapse 元素
+            const triggerButton = `<div class="list-group"  data-toggle="collapse" data-target="#${collapseId}" aria-expanded="false" aria-controls="${collapseId}">${topItem.pageName}</div>`;
+
+            // 返回顶级菜单项的 HTML 内容，包括折叠触发器和子菜单项
+            return triggerButton + subItemsHtml;
+          } else {
+            // 如果顶级菜单项没有子菜单项，则只创建一个 a 标签
+            return `<li class="list-group-item"><a href="#">${topItem.pageName}</a></li>`;
+          }
+        })
+        .join("");
+
+      // 清空 ulShow 元素的内容，并添加新构建的列表组内容
+      $("#ulShow")
+        .empty()
+        .append(`<ul class="list-group">${listGroupContent}</ul>`);
+
+      // 初始化所有 collapse 元素以启用折叠功能
+      $(".collapse").collapse();
+    },
+    error: (xhr, status, error) => {
+      // 处理 AJAX 请求失败的情况
+      console.error("An error occurred:", error);
     },
   });
 }
+
+function handleSubItem(pageName) {
+  console.log(pageName, "pageName");
+}
+
+$(".subItem").on("click", function (pageName) {
+  handleSubItem(pageName);
+});
