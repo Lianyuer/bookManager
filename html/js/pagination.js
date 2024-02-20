00cf1251e5894e1394e868d09004928b4f60d36d
lianyu <1319764245@qq.com>
2024-02-20 13:26:40 +0800

    新增分页页号选中样式与所有切换，新增.nb3数据库备份文件

--- html/js/pagination.js
+++ html/js/pagination.js
@@ -16,17 +16,24 @@ function showLi() {
       ? allData.length / pageSize
       : allData.length / pageSize + 1;
   $(".pagination").empty();
-  var content = `<li class="page-item"><a href="#" class="page-link">&laquo;</a></li>`;
+  var content = `<li class="page-item leftPage"><a href="javascript:leftPage()" class="page-link">&laquo;</a></li>`;
   for (let i = 1; i < pageCount; i++) {
-    content += `<li class="page-item"><a href="#" class="page-link" onclick="choosePage(${i})">${i}</a></li>`;
+    content += `<li class="page-item ${
+      currentPage === i ? "active" : ""
+    }"><a href="javascript:choosePage(${i})" class="page-link" >${i}</a></li>`;
   }
-  content += `<li class="page-item"><a href="#" class="page-link">&raquo;</a></li>`;
+  content += `<li class="page-item rightPage"><a href="javascript:rightPage()" class="page-link">&raquo;</a></li>`;
   $(".pagination").html(content);
 }
 
-// 选择不同的页数展示不同数据
+// 选择不同的页号展示不同数据
 function choosePage(i) {
+  // 移除当前激活的页码样式
+  $(`.page-item.active`).removeClass("active");
   currentPage = i;
+  console.log(currentPage);
+  // 为新的当前页码添加激活样式
+  $(`.page-item:has(a[href="javascript:choosePage(${i})"])`).addClass("active");
   var startIndex = (currentPage - 1) * pageSize;
   var endIndex =
     currentPage * pageSize < allData.length
@@ -37,6 +44,12 @@ function choosePage(i) {
     newData.push(allData[i]);
     showData(newData);
   }
+  // 获取要更新的标签元素
+  const currentPageDom = document.getElementById("currentPageDom");
+  const totalCountDom = document.getElementById("totalCountDom");
+  // 更新标签内容
+  currentPageDom.textContent = `当前第${currentPage}页`;
+  totalCountDom.textContent = `共${allData.length}条`;
 }
 
 function showData(data) {
@@ -56,4 +69,27 @@ function showData(data) {
     </tr>`;
   }
   $(".tShow").html(content);
+}
+
+function leftPage() {
+  if (currentPage > 1) {
+    currentPage--;
+    choosePage(currentPage);
+    document.querySelector(".rightPage").classList.remove("disabled");
+  }
+  if (currentPage == 1) {
+    document.querySelector(".leftPage").classList.add("disabled");
+  }
+}
+
+function rightPage() {
+  // console.log(pageCount, "pageCount");
+  if (currentPage < pageCount) {
+    currentPage++;
+    choosePage(currentPage);
+    document.querySelector(".leftPage").classList.remove("disabled");
+  }
+  if (currentPage > pageCount) {
+    document.querySelector(".rightPage").classList.add("disabled");
+  }
 }
